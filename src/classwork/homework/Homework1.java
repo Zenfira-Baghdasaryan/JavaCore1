3e7122a6bd4d64f30e7e9367d23e53e08dd09096
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-08-28 12:36:22 +0400

    homework

--- src/classwork/homework/Homework1.java
+++ src/classwork/homework/Homework1.java
@@ -5,16 +5,20 @@ public class Homework1 {
      /*  char c ='\n';
         System.out.println("******"+c+"******"+c+"******"+c+"******"+c+"******"+c+"******");
             */
-        System.out.println("******");
+      /*  System.out.println("******");
         System.out.println("******");
         System.out.println("******");
         System.out.println("******");
         System.out.println("******");
         System.out.println("******");
 
+       */
 
-
-
-        }
-}
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }}
+    }
 
