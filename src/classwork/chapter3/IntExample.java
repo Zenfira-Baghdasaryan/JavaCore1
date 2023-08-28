73a27da3805dd25378f14176b2dc4df001ea7ebd
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-08-28 12:03:19 +0400

    homework

---
+++ src/classwork/chapter3/IntExample.java
@@ -0,0 +1,10 @@
+package classwork.chapter3;
+
+public class IntExample {
+    public static void main(String[] args) {
+        String aStr="625";
+int a =Integer.parseInt(aStr);
+        System.out.println(aStr+5);
+
+    }
+}
---
+++ src/classwork/chapter3/StrExample.java
@@ -0,0 +1,16 @@
+package classwork.chapter3;
+
+public class StrExample {
+    public static void main(String[] args) {
+        /*char[]name={'z','e','n','f','i','r','a'};
+        for (int i=0;i< name.length;i++){
+            System.out.print(name[i]);
+            */
+        String name="Zenfira";
+char[]chars=name.toCharArray();
+for(int i=0;i< chars.length;i++){
+    System.out.print(chars [i]);
+        }
+
+    }
+}
