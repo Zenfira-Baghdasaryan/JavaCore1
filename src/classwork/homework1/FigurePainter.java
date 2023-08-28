43c143bc5e6199582a4d7d15028c16c03522ae52
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-08-28 13:59:51 +0400

    homework

--- src/classwork/homework1/FigurePainter.java
+++ src/classwork/homework1/FigurePainter.java
@@ -1,4 +1,58 @@
 package classwork.homework1;
 
 public class FigurePainter {
+    public static void main(String[] args) {
+        for (int i = 1; i < 5; i++) ;
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println();
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+
+        System.out.println();
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+
+        System.out.println();
+        System.out.println("* * * * *");
+        System.out.println("  * * * *");
+        System.out.println("    * * *");
+        System.out.println("      * *");
+        System.out.println("        *");
+
+        System.out.println();
+        System.out.println("    *    ");
+        System.out.println("   * *   ");
+        System.out.println("  * * *  ");
+        System.out.println(" * * * * ");
+        System.out.println("* * * * *");
+        System.out.println(" * * * * ");
+        System.out.println("  * * *  ");
+        System.out.println("   * *  ");
+        System.out.println("    *  ");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
