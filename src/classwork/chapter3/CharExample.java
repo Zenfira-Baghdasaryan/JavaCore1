3b426189199dfd594502e0a290d7eb0038ffbb1f
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-08-28 12:26:03 +0400

    homework

--- src/classwork/chapter3/CharExample.java
+++ src/classwork/chapter3/CharExample.java
@@ -2,10 +2,18 @@ package classwork.chapter3;
 
 public class CharExample {
     public static void main(String[] args) {
-        char a =136;
+     /*   char a =136;
         char b ='-';
         System.out.print(a);
         System.out.print(b);
         System.out.print(a);
+
+      */
+        char b = 10;
+        char a = '@';
+        System.out.println(a + "   " + a+"   "+a+"   "+a);
+        System.out.print("  "+a+"   "+a+"   " + a + b );
+        System.out.println(a+"   "+a+"   "+a+"   "+a);
+
     }
 }
