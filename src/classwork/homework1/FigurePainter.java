18e0b8e4f2db9279c4ae7aea26213ab9d53e327f
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-08-29 07:32:19 +0400

    homework

--- src/classwork/homework1/FigurePainter.java
+++ src/classwork/homework1/FigurePainter.java
@@ -104,46 +104,54 @@ public class FigurePainter {
         System.out.print("  ");
         for (int j = 0; j < 2; j++) {
             System.out.print(" ");
-            System.out.print('*');}
-            System.out.println();
+            System.out.print('*');
+        }
+        System.out.println();
         for (int i = '*'; i < 3; i++) ;
         System.out.print(" ");
         for (int j = 0; j < 3; j++) {
             System.out.print(" ");
-            System.out.print('*');}
+            System.out.print('*');
+        }
 
         System.out.println();
         for (int i = '*'; i < 4; i++) ;
         System.out.print("");
         for (int j = 0; j < 4; j++) {
             System.out.print(" ");
-            System.out.print('*');}
+            System.out.print('*');
+        }
         System.out.println();
         for (int i = '*'; i < 5; i++) ;
         for (int j = 0; j < 5; j++) {
-            System.out.print('*'+" ");}
+            System.out.print('*' + " ");
+        }
         System.out.println();
         for (int i = '*'; i < 4; i++) ;
         System.out.print("");
         for (int j = 0; j < 4; j++) {
             System.out.print(" ");
-            System.out.print('*');}
+            System.out.print('*');
+        }
         System.out.println();
         for (int i = '*'; i < 3; i++) ;
         System.out.print(" ");
         for (int j = 0; j < 3; j++) {
             System.out.print(" ");
-            System.out.print('*');}
+            System.out.print('*');
+        }
         System.out.println();
         for (int i = '*'; i < 2; i++) ;
         System.out.print("  ");
         for (int j = 0; j < 2; j++) {
             System.out.print(" ");
-            System.out.print('*');}
+            System.out.print('*');
+        }
         System.out.println();
         for (int i = '*'; i < 1; i++) ;
         for (int j = 0; j < 1; j++) {
             System.out.print("    ");
             System.out.print('*');
         }
-    }}
+    }
+}
