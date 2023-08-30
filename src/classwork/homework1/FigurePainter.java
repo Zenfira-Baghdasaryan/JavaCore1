db556b39ae5d9dda6f6ecdf9cfb1e0833fe14635
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-08-30 09:36:39 +0400

    homework

--- src/classwork/homework1/FigurePainter.java
+++ src/classwork/homework1/FigurePainter.java
@@ -3,26 +3,29 @@ package classwork.homework1;
 public class FigurePainter {
     public static void main(String[] args) {
 
+        /* homework 1*/
+        for (int i = '*'; i < 5; i++) ;
+        for (int j = 0; j < 5; j++)
+            System.out.print('*' + " ");
+        System.out.println();
+        for (int i = '*'; i < 4; i++) ;
+        for (int j = 0; j < 4; j++)
+            System.out.print('*' + " ");
+        System.out.println();
+        for (int i = '*'; i < 3; i++) ;
+        for (int j = 0; j < 3; j++)
+            System.out.print('*' + " ");
+        System.out.println();
+        for (int i = '*'; i < 2; i++) ;
+        for (int j = 0; j < 2; j++)
+            System.out.print('*' + " ");
+        System.out.println();
+        for (int i = '*'; i < 1; i++) ;
+        for (int j = 0; j < 1; j++)
+            System.out.print('*' + " ");
 
-        for (int i = '*'; i < 5; i++) ;
-        for (int j = 0; j < 5; j++)
-            System.out.print('*' + " ");
-        System.out.println();
-        for (int i = '*'; i < 4; i++) ;
-        for (int j = 0; j < 4; j++)
-            System.out.print('*' + " ");
-        System.out.println();
-        for (int i = '*'; i < 3; i++) ;
-        for (int j = 0; j < 3; j++)
-            System.out.print('*' + " ");
-        System.out.println();
-        for (int i = '*'; i < 2; i++) ;
-        for (int j = 0; j < 2; j++)
-            System.out.print('*' + " ");
-        System.out.println();
-        for (int i = '*'; i < 1; i++) ;
-        for (int j = 0; j < 1; j++)
-            System.out.print('*' + " ");
+
+        /*homework 2*/
         System.out.println();
         System.out.println();
         for (int i = '*'; i < 1; i++) ;
@@ -44,6 +47,9 @@ public class FigurePainter {
         for (int i = '*'; i < 5; i++) ;
         for (int j = 0; j < 5; j++)
             System.out.print('*' + " ");
+
+
+        /*homework 3 */
         System.out.println();
         System.out.println();
         for (int i = '*'; i < 1; i++) ;
@@ -68,6 +74,9 @@ public class FigurePainter {
         for (int i = '*'; i < 5; i++) ;
         for (int j = 0; j < 5; j++)
             System.out.print('*' + " ");
+
+
+        /*homework 4 */
         System.out.println();
         System.out.println();
         for (int i = '*'; i < 5; i++) ;
@@ -93,6 +102,9 @@ public class FigurePainter {
             System.out.println();
             System.out.print("        " + '*');
         }
+
+        
+        /*homework 5 */
         System.out.println();
         for (int i = '*'; i < 1; i++) ;
         for (int j = 0; j < 1; j++) {
