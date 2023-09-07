9be0a2db2982202595a63a7b564ef6d4567a4eac
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-09-07 10:46:28 +0400

    homework

--- src/classwork/homework1/ArrayUtil.java
+++ src/classwork/homework1/ArrayUtil.java
@@ -9,22 +9,15 @@ public class ArrayUtil {
         int[] numbers = {1, 6, 3, 9, 15, 52, -3, 5, 8};
         //1
         for (int i = 0; i < numbers.length; i++) {
-            int sum = 0;
-            sum = sum + numbers[i];
-            System.out.print(sum + " ");
-
+            System.out.print(numbers[i] + " ");
         }
         //2
         System.out.println();
-        System.out.println();
-        System.out.println(numbers[0]);
+        System.out.println("First:" + numbers[0]);
         //3
-        System.out.println();
-        System.out.println(numbers[8]);
+        System.out.println("Last:" + numbers[numbers.length - 1]);
         //4
-        System.out.println();
-        System.out.println(numbers.length);
-        System.out.println();
+        System.out.println("Length is:" + numbers.length);
         //5
         int min = numbers[0];
         for (int i = 0; i < numbers.length; i++) {
@@ -34,43 +27,37 @@ public class ArrayUtil {
         }
         System.out.print("Min:" + min);
         //6
-
-        System.out.println();
-        System.out.println();
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
-        }
-        System.out.println(numbers[4]);
-        System.out.println(sum - 62);
-        //7
-        System.out.println();
-        int count = 0;
-        for (int num : numbers) {
-            if (num % 2 == 0) {
-                count++;
+        if (numbers.length % 2 == 0) {
+            System.out.println(numbers[numbers.length / 2 - 1] + " " + numbers[numbers.length]);
+        } else {
+            System.out.println();
+            System.out.println("middle num:" + numbers[numbers.length / 2]);
+            //7
+            int evenCount = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] % 2 == 0) {
+                    evenCount++;
+                }
             }
-        }
-
-        System.out.println("zuyg:" + count);
-        //8
-        System.out.println();
-        int odd = 0;
-        for (int num : numbers) {
-            if (num % 2 != 0) {
-                odd++;
+            System.out.println("evenCount :" + evenCount);
+            //8
+            int oddCount = 0;
+            for (int num : numbers) {
+                if (num % 2 != 0) {
+                    oddCount++;
+                }
             }
+            System.out.println("oddCount:" + oddCount);
+            //9
+            int sum = 0;
+            for (int i = 0; i < numbers.length; i++) {
+                sum += numbers[i];
+            }
+            System.out.println("Sum:" + sum);
+            //10
+            System.out.println("Avg:" + sum / numbers.length);
         }
-        System.out.println("kent:" + odd);
-        //9
-        System.out.println();
-        System.out.println(sum);
-        //10
-        System.out.println();
-        System.out.println(sum / 9);
     }
-
-
 }
 
 
