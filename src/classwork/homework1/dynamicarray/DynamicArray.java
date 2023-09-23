575acf18395597309a33071ee3a3e68c631e0e38
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-09-23 11:21:58 +0400

    homework

--- src/classwork/homework1/dynamicarray/DynamicArray.java
+++ src/classwork/homework1/dynamicarray/DynamicArray.java
@@ -12,19 +12,49 @@ public class DynamicArray {
     }
 
     void extend() {
-        int[] tmp=new int[array.length+10];
-        for (int i = 0; i <size ; i++) {
-            tmp[i]=array[i];
+        int[] tmp = new int[array.length + 10];
+        for (int i = 0; i < size; i++) {
+            tmp[i] = array[i];
         }
-        array=tmp;
+        array = tmp;
     }
 
     void print() {
         for (int i = 0; i < size; i++) {
             System.out.print(array[i] + " ");
-
         }
+    }
+
+    public void deleteByIndex(int index) {
+        int[] j = new int[10];
+        for (int k = 0; k != index; k++)
+            if (j != array) ;
+        System.out.print("There is no element");
 
     }
+
+    public void setArray(int value, int index) {
+        int[] a = new int[index + 10];
+        for (int i = index; i < array.length; i++) {
+
+        }
+        value = index;
+        System.out.println("There is no element");
+    }
+
+    public boolean exists(int value) {
+        if (value != size) {
+            System.out.println("false");
+        } else {
+            System.out.println("true");
+        }
+        return false;
+    }
+
+    public int getIndexByValue(int value) {
+        if (value != size) ;
+        return 0;
+    }
 }
+
 
--- src/classwork/homework1/dynamicarray/DynamicArrayDemo.java
+++ src/classwork/homework1/dynamicarray/DynamicArrayDemo.java
@@ -1,5 +1,8 @@
 package classwork.homework1.dynamicarray;
 
+import java.io.File;
+import java.sql.Array;
+
 public class DynamicArrayDemo {
     public static void main(String[] args) {
         DynamicArray dy = new DynamicArray();
@@ -7,8 +10,16 @@ public class DynamicArrayDemo {
         dy.add(5);
         dy.add(8);
         dy.add(9);
-        dy.add(12);
+        dy.add(10);
         dy.print();
-
+        System.out.println();
+        new DynamicArray().deleteByIndex(7);
+        System.out.println();
+        new DynamicArray().setArray(6, 789);
+        System.out.println();
+        new DynamicArray().exists(13);
+        System.out.println();
+        new DynamicArray().getIndexByValue(9);
+        System.out.println();
     }
 }
