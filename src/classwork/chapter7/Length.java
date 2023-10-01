88d9d69a3527acdca1d7995e42dd297b4b8beddf
Zenfira Baghdasaryan <zenfira.b00@mail.com>
2023-10-01 07:29:45 +0400

    homework

---
+++ src/classwork/chapter7/Length.java
@@ -0,0 +1,12 @@
+package classwork.chapter7;
+
+public class Length {
+    public static void main(String[] args) {
+        int a[]=new int[10];
+        int b[]={3,5,7,1,8,9,4,0};
+        int c[]={65,34,8,76};
+        System.out.println("length of a is"+a.length);
+        System.out.println("length of b is"+b.length);
+        System.out.println("length of c is"+c.length);
+    }
+}
